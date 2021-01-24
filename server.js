e5a9619c919b249cd61a90d8552e4a5ce85ed3e2
Glitch (hello-express) <none>
2021-01-24 16:01:28 +0000

    🕴📍 Checkpoint ./server.js:26244623/3334

--- server.js
+++ server.js
@@ -2,10 +2,7 @@
 const express = require("express");
 const app = express();
 let db = require("quick.db");
-let database = db.get(`database`);
-let dbs = db.get(`guild`)
-
-if (!database) database = 0;
+const mongoose = require("mongoose")
 
 app.use(express.static("public"));
 
@@ -14,12 +11,6 @@ app.get("/", (request, response) => {
 });
 
 app.get("/:code", async (req, res) => {
-  if (!database) return res.json({error: "Url not found."})
-  
-  let find = await db.get(`database.${req.params.code}`);
-  if (!find) return res.json({error: "Cannot find this code"})
-  
-  res.redirect(find.redirect);
 })
 
 const listener = app.listen(process.env.PORT, () => {
@@ -33,7 +24,7 @@ let client = new Discord.Client({
 });
 
 client.on("ready", () => {
-  console.log(`Ready to handle ${database.length || 0} link`)
+  console.log(`Ready to handle all url`)
 });
 
 // Bot
@@ -43,13 +34,11 @@ let config = process.env
 client.login(config.token)
 
 client.on("guildRemove", async guild => {
-  let find = dbs.find(x => x === guild.id);
-  if (!find) return;
   
-  let find2 = db.find(x => x == find.code);
-  if (!find2) return;
-  
-  db.delete(`guild.${guild.id}`);
-  db.delete(`database.${find2.code}`);
-  
+});
+
+// connect to mongodb
+mongoose.connect(config.mongo, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
