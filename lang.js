bd979de7cdd0cc316b1a107be849b1c8e414a7ba
Glitch (hello-express) <none>
2021-01-25 16:58:15 +0000

    🚨🏇 Checkpoint ./server.js:26244623/39544 ./lang.js:26244623/55

--- lang.js
+++ lang.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 module.exports = model(
   "lang",
   new Schema({
+    guild: String,
     lang: String
   })
 );
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const color = "#05eeff"
 const passport =  require('passport');
 const session = require("express-session");
 const  Strategy = require("passport-discord").Strategy
+const lang = require("./lang.js");
 
 // Discord Login
 passport.serializeUser(function(user, done) {
@@ -156,11 +157,18 @@ let config = process.env;
 
 client.login(config.token);
 
+client.on("guildCreate", async guild => {
+  
+  let newLang = new lang({
+    guild: guild.id,
+    lang: "en"
+  })
+  
+});
+
 client.on("guildRemove", async guild => {
   
-  db.findOne({ guild_id: guild.id }, async (err, data) => {
-    if (err) return;
-    
+  db.findOne({ guild_id: guild.id }, async (err, data) => {    
     if (data) {
       data.remove()
     } else {
@@ -190,6 +198,12 @@ client.on("message", async message => {
     .split(/ +/g);
   const cmd = args.shift().toLowerCase();
   
+
+  lang.findOne({guild: message.guild.id}, async (err, data) => {
+    
+    if (data) {
+//       EN
+      if (data === "en") {
   if (cmd === "help" || cmd === "h" || cmd === "commands") {
     const embed = new Discord.MessageEmbed()
     .setAuthor('Make your own invite url', client.user.displayAvatarURL())
@@ -400,7 +414,239 @@ client.on("message", async message => {
       }
       
     })
+  }       
+        
+//         ID
+      } else
+//       ID
+        if (data === "id") {
+  if (cmd === "help" || cmd === "h" || cmd === "commands") {
+    const embed = new Discord.MessageEmbed()
+    .setAuthor('Make your own invite url', client.user.displayAvatarURL())
+    .setColor(color)
+    .addField(`${prefix}help`, 'Show this command, and commands list')
+    .addField(`${prefix}set [channel]`, 'Set this server invite url channel')
+    .addField(`${prefix}edit [new_code]`, 'Edit this server custom invite code')
+    .addField(`${prefix}unsetup`, 'Unsetup this server custom invite url or delete')
+    .addField(`${prefix}setup <channel>`, 'Setup the own invite url')
+    .addField(`${prefix}link`, 'Get this server own invite link')
+    .addField(`${prefix}tutorial`, 'Get the tutorial embed')
+    .addField(`${prefix}information`, 'Get this server url info')
+    .setFooter(`©️ URLCORD.CF - 2021 | [] required, <> optional`)
+    
+    message.channel.send(embed);
+  };
+  
+  if (cmd === "set" || cmd === "channel") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Error: You need Administrator permission");
+    
+    let channel = message.mentions.channels.first();
+    if (!channel) return message.channel.send(`Error: You must mentions channel first.`);
+    
+    if (!message.guild.me.hasPermission("CREATE_INSTANT_INVITE")) return message.channel.send("Error: Im need Create Invite permission");
+    
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+      
+      if (data) {
+    const link = await channel.createInvite({maxAge: 0, maxUses: 0});
+    
+    message.channel.send(`Succes: This server invite url channel has been set to ${channel}`);
+        data.guild.redirect = `https://discord.gg/${link.code}`
+        data.save()
+      } else {        
+        return message.channel.send(`Error: This server do not setup, use **${prefix}setup** first.`)        
+      }
+      
+    });
+  
+  };
+  
+  if (cmd === "edit" || cmd === "code") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Error: You need Administrator permission')
+    
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+      if (data) {
+        let newCode = args[0];
+        if (!newCode) return message.channel.send(`Error: Please provide new code`)
+        
+        db.findOne({code: newCode}, async (err, datas) => {
+          
+          if (datas) {
+            return message.channel.send(`Error: This code (**${newCode}**) already have guild, try another code`)
+          } else {
+            
+            data.code = newCode;
+            data.guild.code = newCode;
+            data.save();
+            
+            return message.channel.send(`Succes: Succesfully set server invite to https://urlcord.cf/${newCode}`)
+            
+          }
+          
+        });
+        
+      } else {
+        return message.channel.send(`Error: This server do not setup custom invite link`)
+      }
+    });
+    
   }
+  
+  if (cmd === "setup" || cmd === "on" || cmd === "enable") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Error: You need Administrator permission");
+    
+    const channel = message.mentions.channels.first() || message.channel;
+    
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+      
+      if (data) {
+        return message.channel.send(`Error: This server already setup.`)
+      } else {
+        
+        let code = "server" + makeid(2);
+        db.findOne({code: code}, async (err, data) => {
+          
+          if (data) {
+        const link = await channel.createInvite({maxAge: 0, maxUses: 0});
+        let codeS = "server" + makeid(2);
+            
+        let newData = new db({
+          guild_id: message.guild.id,
+          code: codeS,
+          used: 0,
+          guild: {id: message.guild.id, code: code, redirect: `https://discord.gg/${link.code}`},
+          user: {id: message.author.id, tag: message.author.tag, username: message.author.username}
+        });
+        
+        newData.save();
+        
+        message.channel.send(`Succes: Succesfully setup, the default url is https://urlcord.cf/${code} you can edit with **${prefix}edit <new_code>**`);            
+          } else {
+        const link = await channel.createInvite({maxAge: 0, maxUses: 0});
+        
+        let newData = new db({
+          guild_id: message.guild.id,
+          code: code,
+          used: 0,
+          guild: {id: message.guild.id, code: code, redirect: `https://discord.gg/${link.code}`},
+          user: {id: message.author.id, tag: message.author.tag, username: message.author.username}
+        });
+        
+        newData.save();
+        
+        message.channel.send(`Succes: Succesfully setup, the default url is https://urlcord.cf/${code} you can edit with **${prefix}edit <new_code>**`);            
+          }
+          
+        }); 
+        
+      }
+      
+    });
+    
+    
+  };
+  
+  if (cmd === "unsetup" || cmd === "off" || cmd === "disable") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channnel.send("Error: You need Administrator permission");
+    
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+      
+      if (data) {
+        
+        
+        data.remove();
+        
+        return message.channel.send(`Succes: Succesfully unsetup`)
+        
+      } else {
+        
+        return message.channel.send(`Error: This server do not setup`)
+        
+      }
+      
+    });
+  }
+  
+  if (cmd === "link" || cmd.toLowerCase() === message.guild.name.toLowerCase()) {
+    
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+      
+      if (data) {
+        return message.channel.send(`here: https://urlcord.cf/${data.guild.code}\nsetup by: ${data.user.tag} [${data.user.id}]`)
+      } else {
+        return message.channel.send(`Error: This server do not setup`)
+      }
+      
+    })
+    
+  }
+  
+  if (cmd === "tutorial" || cmd === "howto") {
+    const embed = new Discord.MessageEmbed()
+    .setAuthor('How to tutorial', client.user.displayAvatarURL())
+    .setColor(color)
+    .addField("Step 1", `add me to your server with [this link](https://urlcord.cf/invite)`)
+    .addField("Step 2", `give me create invite permission`)
+    .addField("Step 3", `type **${prefix}setup <channel>** and you will got default invite url`)
+    .addField("Step 4", `and you can edit with **${prefix}edit <new_code>**`)
+    .addField("Step 5", `enjoy you custom invite url :D`)
+    .setFooter(`add me on https://urlcord.cf`)
+    
+    message.channel.send(embed)
+  }
+  
+  if (cmd === "info" || cmd === "information" || cmd === "own") {
+    db.findOne({guild_id: message.guild.id}, async (err, data) => {
+
+      if (data) {
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`${message.guild.name} Invite link information`, message.guild.iconURL())
+    .setDescription(`This server invite link is https://urlcord.cf/${data.guild.code}`)
+    .addField(`JS Structures`, `\`\`\`js
+    {
+    guild_id: ${data.guild.id},
+    code: ${data.code},
+    used: ${data.used},
+    guild: {id: ${data.guild.id}, code: ${data.guild.code}, redirect: ${data.guild.redirect}}},
+    user: {id: ${data.user.id}, tag: ${data.user.tag}, username: ${data.user.username}}
+    }
+    \`\`\``)
+    .addField(`Normat Stuctures`, `\`\`\`
+    guild_id: ${data.guild.id}
+    code: ${data.code}
+    used: ${data.used}x
+    guild.id: ${data.guild.id}
+    guild.code: ${data.guild.code}
+    guild.redirect: ${data.guild.redirect}
+    user.id: ${data.user.id}
+    user.tag: ${data.user.tag}
+    user.username: ${data.user.username}
+    \`\`\``)
+    .setColor(color)
+    
+    return message.channel.send(embed)
+      } else {
+        return message.channel.send(`Error: This server do not setup`)
+      }
+      
+    })
+  }        
+      } else
+        {
+          
+          let newLang = new lang({
+            guild: message.guild.id,
+            lang: "lang"
+          })
+          return message.channel.send(`Try again to use this commands, Error: Fail to load language`);
+        }
+      
+    } else {
+      
+    }
+    
+  });
+  
 });
 
 // global function
